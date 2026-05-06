59373ad064d32a7566aa3597f360b24e75eb0992
GERMAN ISAAC <isaacluna752@gmail.com>
2026-05-06 01:59:36 -0500

    mejor UI

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ZAPATERÍA PRO',
-      home: Scaffold(
-        appBar: AppBar(title: const Text("ZAPATERÍA PRO")),
-        body: const Center(
-          child: Text("APK DESDE LA NUBE 🚀", style: TextStyle(fontSize: 22)),
+      debugShowCheckedModeBanner: false,
+      title: 'Zapatería PRO',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int ventas = 0;
+  int stock = 10;
+
+  void vender() {
+    if (stock > 0) {
+      setState(() {
+        stock--;
+        ventas++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ZAPATERÍA PRO"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                title: const Text("Ventas"),
+                trailing: Text("$ventas"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: const Text("Stock"),
+                trailing: Text("$stock"),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: vender,
+              child: const Text("VENDER"),
+            )
+          ],
         ),
       ),
     );
   }
-}
+}}
